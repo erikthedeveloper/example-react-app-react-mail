0156e6d333b444719a8cdbd1b9a98e1cc3fdcd8f
Erik Aybar <erikthedeveloper@gmail.com>
2016-03-29 07:30:36 -0600

    Update index.{html,js} bulma +(basic styles)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import {render} from 'react-dom'
+require('./app.scss');
+// If we wanted to polyfill the fetch API (recommended).
+// https://github.com/github/fetch
+// require('whatwg-fetch');
+import React from 'react';
+import {render} from 'react-dom';
 
-import App from './App'
+import { App } from './App';
 
-render(<App/>, document.querySelector('#app'))
+render(<App/>, document.querySelector('#app'));
--- tests/App-test.js
+++ tests/App-test.js
@@ -5,19 +5,5 @@ import {render, unmountComponentAtNode} from 'react-dom'
 import App from 'src/App'
 
 describe('App component', () => {
-  let node
-
-  beforeEach(() => {
-    node = document.createElement('div')
-  })
-
-  afterEach(() => {
-    unmountComponentAtNode(node)
-  })
-
-  it('displays a welcome message', () => {
-    render(<App/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React')
-    })
-  })
-})
+  it('does all the things!');
+});
