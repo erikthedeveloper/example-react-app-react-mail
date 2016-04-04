fb9f573c4d98604a6e6a0dc23a4125120c855187
Erik Aybar <erikthedeveloper@gmail.com>
2016-04-04 07:09:37 -0600

    Move to browserHistory (nwb had [hides] this by default)
    After much pain... so much pain...

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { App } from './App';
 import { MessageBrowser } from './components/MessageBrowser';
 import { MessageRoute } from './components/MessageRoute';
 
 export const routes = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={MessageBrowser} />
       <Route path=":id" component={MessageRoute} />
