48bc976782fd2c388594f51a76a68de8bc7f7415
Erik Aybar <erikthedeveloper@gmail.com>
2016-04-21 06:36:23 -0600

    MessageList animations! ReactCSSTransitionGroup :sparkles:
    - npm install --save react-addons-css-transition-group
      - See: https://facebook.github.io/react/docs/animation.html
    - Wrap MessageList items in ReactCSSTransitionGroup
      - Set "known" height of 110px in animation.scss <-- TODO: How to work with unknown height for collapse animations?
    - Split out app.scss into {general,animations}.scss

--- src/components/MessageBrowser/MessageList.js
+++ src/components/MessageBrowser/MessageList.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import classnames from 'classnames';
 import { MessageListItem } from './MessageListItem';
 
 export const MessageList = (props) => {
   return (
     <div>
-      {props.messages.map(message => (
-        <MessageListItem
-          key={message.id}
-          deleteMessage={() => props.deleteMessage(message.id)}
-          toggleFlagged={() => props.toggleMessageFlagged(message.id)}
-          message={message}
-        />
-      ))}
+      <ReactCSSTransitionGroup
+        transitionName="animated-list-item"
+        transitionEnterTimeout={250}
+        transitionLeaveTimeout={250}
+      >
+        {props.messages.map(message => (
+          <div key={message.id}>
+            <MessageListItem
+              deleteMessage={() => props.deleteMessage(message.id)}
+              toggleFlagged={() => props.toggleMessageFlagged(message.id)}
+              message={message}
+            />
+          </div>
+        ))}
+      </ReactCSSTransitionGroup>
       <div className="is-text-centered">
         <hr />
         <button
